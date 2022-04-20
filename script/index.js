ea69663ce4dd2bc534b9b09ac8a3851cee8e2d34
BuildTools <unconfigured@null.spigotmc.org>
2022-04-20 11:52:27 +0200

    rotation with gif

--- script/index.js
+++ script/index.js
@@ -1,6 +1,5 @@
 let watchme = document.getElementById('watchme')
 let watchme2 = document.getElementById('watchme2')
-let watchme3 = document.getElementById('watchme3')
 
 watchme.className = 'slidein'
 const listener = (e) => {
@@ -9,20 +8,4 @@ const listener = (e) => {
 watchme2.className = 'slidein2'
 const listener2 = (e) => {
   watchme2.className = 'slidein2 stopped'
-}
-
-rotation(watchme3);
-
-async function rotation(img){
-    n = 0;
-    while (true){
-      img.src = "../img/rotation/" + n + ".png";
-      await sleep(100);
-      n = (n+1)%19;
-    }
-}
-
-//fonction qui fait une pause de x miliseconde
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
 }
